677ede5d5c8728da91232421d999486131073d50
CANewbould <newbould391@btinternet.com>
2022-01-11 21:28:17 +0000

    v 4.0.5.1

--- creole.ex
+++ creole.ex
@@ -1,18 +1,16 @@
 --------------------------------------------------------------------------------
 -- creole.ex
 --------------------------------------------------------------------------------
--- Version 4.0.5.0
+-- Version 4.0.5.1
 -- Date: 2022.01.03
 -- Author: C A Newbould
 -- Status: operational
 -- Changes:
---* created
---* fully-documented
---* tested
---* revised after transfer of material to 'library/creole.e'
+--* altered by virtue of changed default in //creole.e//
+--* more documentation
 --------------------------------------------------------------------------------
--- This application is a proof-of-concept: the target "screen" is
--- a webbrowser tab rather than a terminal window.
+-- This application demonstrates a way of changing the target "screen" to
+-- a webbrowser tab rather than using a pop-up terminal window.
 --
 -- This approach uses the creole markup language to construct and lay out the
 -- output of an Open Euphoria application.
@@ -28,10 +26,10 @@
 -- preference is expressed.
 --------------------------------------------------------------------------------
 -- The action starts here:
---* open the support library
+--* include the support library
 --* start to write output using the dedicated functions in the library
 --
--- These functions sent output to a '.cr' file rather than to
+-- These functions send output to a '.cr' file rather than to
 -- the standard output stream, but are largely similar in effect:
 -- being based on the three OE mainstays of 'puts', 'print' and 'printf'.
 --------------------------------------------------------------------------------
@@ -42,7 +40,7 @@ include library/creole.e -- for 'display', 'prnt', 'write', 'writef'
 -- styles.
 --------------------------------------------------------------------------------
 write("=Testing", 1) -- using creole heading
-write("==Using list.e", 1) -- also using creole heading
+write("==Using list.e", 1) -- also using a creole heading
 --------------------------------------------------------------------------------
 -- Define raw data for this particular illustration
 --------------------------------------------------------------------------------
@@ -78,29 +76,40 @@ write("##head_##(//list//) -> ", 0) prnt(head_(l)) -- this is the CANOE version
 -- We have now completed our output, so we want to:
 --
 --* finish off
+--* choose our destination browser
 --* display our result, using the 'display' function
 --
--- We can change the browser name, using an appropriate parameter value here)
+-- This function closes the output stream (of our creole-based material) and shows
+-- the result in the browser tab.
+--
+-- The default browser is, in fact, part of the IUP GUI, but you can use whatever
+-- browser you have on your system.
+--
+-- To change the browser use an appropriate parameter value in ##Display##.
+-- It should be the **string** name as recognised by the current OS
+-- (for example "firefox", "safari" or, in MS Windows, "start" which will call
+-- the default system browser).
 --
 -- The function will do its housekeeping and then open a new browser tab,
--- containing the output in html style
+-- containing the output in html style.
 --
--- The system may issue browser-related messages to the Terminal window
+-- The system may issue browser-related messages to the Terminal window; these
+-- can safely be ignored.
 --
 -- Two files are left in the folder from which this app is called:
 --
 --* test.cr
 --* test.html
 --
--- the former holding the markup code; the latter the html.
+-- the former holding the intermediate markup code; the latter the html.
 --
 -- This app continues to overwrite these files, so there is no especial need
 -- to delete them each time.
 --
--- They could be useful in the event of an error in the output, as both are
+-- They could be even useful in the event of an error in the output, as both are
 -- perfectly readable.
 --------------------------------------------------------------------------------
-display() -- a [string] parameter will change the browser used
+display() -- with the "IUP" default: a [string] parameter will change the browser used
 --------------------------------------------------------------------------------
 -- NB: STDERR is now clear for its proper purpose, as shown here
 --------------------------------------------------------------------------------
@@ -109,4 +118,13 @@ warn(888)
 --------------------------------------------------------------------------------
 -- Previous versions
 --------------------------------------------------------------------------------
+-- Version 4.0.5.0
+-- Date: 2022.01.03
+-- Author: C A Newbould
+-- Status: operational
+-- Changes:
+--* created
+--* fully-documented
+--* tested
+--* revised after transfer of material to 'library/creole.e'
 --------------------------------------------------------------------------------
--- library/address.e
+++ library/address.e
@@ -4,14 +4,13 @@
 --/*
 --
 --= Open Euphoria address library
---* Version: 4.0.5.0
+--* Version: 4.0.5.1
 --* Author: C A Newbould
---* Date: 2022.01.02
+--* Date: 2022.01.10
 --* Status: incomplete
 --* Changes:
 --** created
---** **address** defined
---** ##Address## defined
+--** ##free## defined
 --
 --==Open Euphoria extension library: address
 --This library contains tools that apply to the **address*
@@ -23,21 +22,37 @@
 --
 --*/
 --------------------------------------------------------------------------------
-constant M_ALLOC = 16, NULL = 0
+constant M_ALLOC = 16, M_FREE = 17, NULL = 0, VOID = 0
 --------------------------------------------------------------------------------
 --/*
 ----
 --*/
 --------------------------------------------------------------------------------
-export type address(atom a) -- (a) -> addr
+export type address(atom a) -- t(a) -> addr
     return a > 0
     end type
-    export function Address(atom a) -- (a) -> addr
+    export function Address(atom a) -- f(a) -> addr
         if a > 0 then return machine_func(M_ALLOC, a)
         else return NULL
         end if
     end function
+    export function free(address a) -- f(addr) -> v
+        machine_proc(M_FREE, a)
+        return VOID
+    end function
+--------------------------------------------------------------------------------
+--/*
+--    <eucode>global function poke(address a, object o) -- f(addr -> o) -> v</eucode>    
+--*/
 --------------------------------------------------------------------------------
 -- Previous versions
 --------------------------------------------------------------------------------
+--* Version: 4.0.5.0
+--* Author: C A Newbould
+--* Date: 2022.01.02
+--* Status: incomplete
+--* Changes:
+--** created
+--** **address** defined
+--** ##Address## defined
 --------------------------------------------------------------------------------
