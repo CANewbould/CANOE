f870c920e4979a65f88b7059ecbdac5b9a273f14
CANewbould <newbould391@btinternet.com>
2022-01-26 21:05:01 +0000

    Changed to creole output

--- fizzbuzz.ex
+++ fizzbuzz.ex
@@ -5,23 +5,32 @@
 -- modified to work on any pair of integers
 
 include library/boolean.e -- for 'iif'
-include library/io.e -- for 'writef','writeln'
 include library/ints.e -- for 'ints','Ints'
 include library/list.e -- for 'product'
 include library/sequence.e -- for 'map','map_'
 include library/string.e -- for 'string'
+include library/creole.e -- for 'display','write','writef'
 
 constant i = Ints(1,100)
-writeln("Simple solution")
-map(i,"writef","%d ") writeln()
+write("=Fizz/buzz test program")
+write("==Simple solution")
+map(i[1..20],"writef","%d ") write()
+map(i[21..40],"writef","%d ") write()
+map(i[41..60],"writef","%d ") write()
+map(i[61..80],"writef","%d ") write()
+map(i[81..100],"writef","%d ") write()
 
-writeln("Fizz solution")
+write("==Fizz solution")
 function fizz(integer i,integer j)
     return iif(remainder(i,j)=0,"fizz",sprintf("%d",i))
 end function
-map(map(i,"fizz",3),"writef","%s ") writeln()
+map(map(i[1..20],"fizz",3),"writef","%s ") write()
+map(map(i[21..40],"fizz",3),"writef","%s ") write()
+map(map(i[41..60],"fizz",3),"writef","%s ") write()
+map(map(i[61..80],"fizz",3),"writef","%s ") write()
+map(map(i[81..100],"fizz",3),"writef","%s ") write()
 
-writeln("Buzz solution")
+write("==Buzz solution")
 function fizzbuzz(integer i,ints j)
     if remainder(i,product(j))=0 then return "fizzbuzz"
     elsif remainder(i,j[1])=0 then return "fizz"
@@ -29,4 +38,12 @@ function fizzbuzz(integer i,ints j)
     else return sprintf("%d",i)
     end if
 end function
-map(map(i,"fizzbuzz",{3,5}),"writef","%s ") writeln()
+map(map(i[1..18],"fizzbuzz",{3,5}),"writef","%s ") write()
+map(map(i[19..36],"fizzbuzz",{3,5}),"writef","%s ") write()
+map(map(i[37..48],"fizzbuzz",{3,5}),"writef","%s ") write()
+map(map(i[49..64],"fizzbuzz",{3,5}),"writef","%s ") write()
+map(map(i[65..80],"fizzbuzz",{3,5}),"writef","%s ") write()
+map(map(i[81..96],"fizzbuzz",{3,5}),"writef","%s ") write()
+map(map(i[97..100],"fizzbuzz",{3,5}),"writef","%s ") write()
+
+display()
