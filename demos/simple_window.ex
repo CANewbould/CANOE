92cab55f0fef7095a06ec1340dfc6d69a69549da
CANewbould <newbould391@btinternet.com>
2022-03-13 22:24:32 +0000

    v 4.1.0.1

--- demos/simple_window.ex
+++ demos/simple_window.ex
@@ -1,12 +1,12 @@
 --------------------------------------------------------------------------------
 -- Test application: simple_window.ex
 --------------------------------------------------------------------------------
---Version: 4.1.0.0
---Date: 2022.03.09
+--Version: 4.1.0.1
+--Date: 2022.03.13
 --Author: C A Newbould
 --Status: Complete
 --Changes:
---* created
+--* tested ##Open## call
 --------------------------------------------------------------------------------
 include libiup.e
 --------------------------------------------------------------------------------
@@ -15,16 +15,16 @@ include libiup.e
 function click_cb(atom self)
     clicks += 1
     attrib(lbl,"TITLE",sprintf("clicked %d times",{clicks}))
-    return DEFAULT
+    return IUP_DEFAULT
 end function
 --------------------------------------------------------------------------------
 -- Main run
 --------------------------------------------------------------------------------
-Open()
+if Open() = IUP_ERROR then abort(0) end if
     integer clicks = 0
     constant lbl = Label("There have been no clicks yet")
     constant btn = Button("Click me")
-    setCallback(btn, "ACTION", Icallback("click_cb"))
+    setcallback(btn, "ACTION", Icallback("click_cb"))
     constant vbox = Vbox({lbl, Hbox({Fill(),btn,Fill()})})
     constant dlg = New(Dialog(vbox),"DIALOGFRAME=YES,MARGIN=10x10, GAP=10, RASTERSIZE=400x0")
     attrib(dlg, "TITLE", "Simple windowed application")
@@ -34,4 +34,10 @@ Close()
 --------------------------------------------------------------------------------
 -- Previous versions
 --------------------------------------------------------------------------------
+--Version: 4.1.0.0
+--Date: 2022.03.09
+--Author: C A Newbould
+--Status: Complete
+--Changes:
+--* created
 --------------------------------------------------------------------------------
