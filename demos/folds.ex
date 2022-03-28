31115eea4772bdec6c5f7db25ad728dd9df7f19b
CANewbould <newbould391@btinternet.com>
2022-03-28 16:04:17 +0100

    v 4.1.0.2

--- demos/folds.ex
+++ demos/folds.ex
@@ -1,12 +1,15 @@
 --------------------------------------------------------------------------------
 -- Test application: folds.ex
 --------------------------------------------------------------------------------
---Version: 4.1.0.0
---Date: 2022.03.09
+--Version: 4.1.0.2
+--Date: 2022.03.28
 --Author: C A Newbould
 --Status: Complete
 --Changes:
---* added ##filter## test
+--* added annotations to functions
+--* changed ##foldr## to ##foldl" for testing
+--* small change to the title
+--* added an example of OE sequence arithmetic
 --------------------------------------------------------------------------------
 -- Determine output mode
 --------------------------------------------------------------------------------
@@ -14,31 +17,31 @@ with define IUPWB
 --------------------------------------------------------------------------------
 -- The source
 --------------------------------------------------------------------------------
-include sequence.e
+include sequence.e -- for 'range'
 sequence list = range(10)
 --------------------------------------------------------------------------------
 -- Start output
 --------------------------------------------------------------------------------
-include io.e
+include io.e -- for 'writefln','write','writel','writeln'
 write("<h1>The list</h1>")
 writel(list)
 --------------------------------------------------------------------------------
 -- Define the fold,map and filter functions
 --------------------------------------------------------------------------------
-function sq(atom a,sequence b)
+function sq(atom a,sequence b) -- a->s->s
     return b&a*a
 end function
 --------------------------------------------------------------------------------
-function add(atom a, atom b)
+function add(atom a, atom b) -- a->a->a
     return b+a
 end function
 include boolean.e
 --------------------------------------------------------------------------------
-function even(atom a,sequence b)
+function even(atom a,sequence b) -- a->s->s
     return iif(remainder(a,2),b,b&a)
 end function
 --------------------------------------------------------------------------------
-function gt(atom a,atom b)
+function gt(atom a,atom b) -- a->a->b
     return a>b
 end function
 --------------------------------------------------------------------------------
@@ -48,7 +51,7 @@ write("foldr(list,\"sq\")-> ")
 writel(foldr(list,"sq"))
 --------------------------------------------------------------------------------
 write("<h2>Sum of the elements</h2>")
-writefln(foldr(list,"add",0),"foldr(list,\"add\",0)-> %d")
+writefln(foldl(list,"add",0),"foldl(list,\"add\",0)-> %d")
 --------------------------------------------------------------------------------
 writeln("<h2>The even elements</h2>")
 write("foldr(list,\"even\")-> ")
@@ -56,9 +59,23 @@ writel(foldr(list,"even"))
 writeln("<h2>The elements > 3</h2><p>filter(list,\"gt\",3)->")
 writel(filter(list,"gt",3))
 --------------------------------------------------------------------------------
-display("Sequences: Showing how foldr does maps and filters, too!")
+function positive(integer a,sequence b)
+    return iif(a,b&a,b)
+end function
+--------------------------------------------------------------------------------
+write("<p>Another way:\nfoldr((list>3)*list,\"positive\")-> ")
+writel(foldr((list>3)*list,"positive"))
+--------------------------------------------------------------------------------
+display("Sequences: Showing how folds do maps and filters, too!")
 --------------------------------------------------------------------------------
 -- Previous versions
+--------------------------------------------------------------------------------
+--Version: 4.1.0.1
+--Date: 2022.03.09
+--Author: C A Newbould
+--Status: Complete
+--Changes:
+--* added ##filter## test
 --------------------------------------------------------------------------------
 --Version: 4.1.0.0
 --Date: 2022.03.02
